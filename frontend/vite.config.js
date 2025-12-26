86189fc946902398cbca95301af909b80961dcde
karthickraja271104 <karthickraja66812@gmail.com>
2025-12-26 18:34:22 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: __dirname,
+  root: '.',
   plugins: [react()],
   server: {
     proxy: {
